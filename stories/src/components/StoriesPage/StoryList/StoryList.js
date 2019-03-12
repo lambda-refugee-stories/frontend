880839947b039d9847c8c0cabc533cc48f851773
James Basile <jbasile145@gmail.com>
2019-03-12 17:47:06 -0400

    added scss dependencies, linked storylist.js to backend, added scss files

--- stories/src/components/StoriesPage/StoryList/StoryList.js
+++ stories/src/components/StoriesPage/StoryList/StoryList.js
@@ -4,58 +4,20 @@ import { Link } from 'react-router-dom';
 import Story from '../Story/Story';
 
 
-const dummyData = [
-    {
-        name: 'James',
-        id: 1,
-        title: 'Story from James',
-        imageurl: '#',
-        story: 'story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story story',
-        isapproved: true
-    },
-    {
-        name: 'Nicole',
-        id: 2,
-        title: 'Story from Nicole',
-        imageurl: '#',
-        story: 'story story story story story story story story story story story story story story story story story story story story story story story story',
-        isapproved: true
-    },
-    {
-        name: 'Rachel',
-        id: 3,
-        title: 'Story from Rachel',
-        imageurl: '#',
-        story: 'story story story story story story story story story story story story story story story story story story story story story story story story',
-        isapproved: true
-    },
-    {
-        name: 'Daniela',
-        id: 4,
-        title: 'Story from Daniela',
-        imageurl: '#',
-        story: 'story story story story story story story story story story story story story story story story story story story story story story story story',
-        isapproved: false
-    },
-    {
-        name: 'Amarachi',
-        id: 5,
-        title: 'Story from Amarachi',
-        imageurl: '#',
-        story: 'story story story story story story story story story story story story story story story story story story story story story story story story',
-        isapproved: false
-    }
-
-]
-
-
 
 export default class StoryList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            stories: dummyData
+            stories: []
         };
+    }
+    
+    componentDidMount() {
+        axios
+        .get('https://refugee-stories-backend-rkolk.herokuapp.com/stories')
+        .then(res => this.setState({ stories: res.data }))
+        .catch(err => console.error(err));
     }
 
 
