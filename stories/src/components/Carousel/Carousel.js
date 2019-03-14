772b7871b7817d67c591d12a2d8f3a80eebd7ee3
James Basile <jbasile145@gmail.com>
2019-03-14 12:11:30 -0400

    added a carousel, switched stories back to collapse feature, added a media query for navbar

---
+++ stories/src/components/Carousel/Carousel.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+  {
+    id: 1,
+    imgUrl: 'https://i.imgur.com/kMT9xpn.jpg',
+    altText: 'Slide 1',
+    caption: 'Where Your Story is OUR Story',
+    header: 'Welcome to Our Stories Page'
+  },
+  {
+    id: 2,
+    imgURL: '',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'HEADER'
+  },
+  {
+    id: 3,
+    imgUrl: '',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'HEADER'
+  }
+];
+
+class StoryCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          className="custom-tag"
+          tag="div"
+          key={item.id}
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+        >
+          <img  className="carousel-img" src={item.imgUrl} alt={item.altText} />
+          <CarouselCaption className="text" captionText={item.caption} captionHeader={item.header} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <div>
+        <style>
+          {
+            `.custom-tag {
+                max-width: 100%;
+                height: 500px;
+                background: black;
+              }`
+          }
+        </style>
+        <Carousel
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+        >
+          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default StoryCarousel;
