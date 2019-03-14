94db8a8d930afdf19bb4dc251f6fa2bd12dcd9c4
Nicole Gunther <nicolemgunther@gmail.com>
2019-03-13 21:21:10 -0700

    fixed update function, adjusted render for submissions

--- stories/src/components/StorySubmissions/EachSubmission.js
+++ stories/src/components/StorySubmissions/EachSubmission.js
@@ -2,24 +2,61 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const EachSubmission = props => {
-    return (
-        <div className='story-container'>
-            <div className='story-header'>
-                <div className='author-img-container'>
-                    <img className='author-img' src={props.story.imageurl} alt='Author' />
-                </div>
-                <h4>{props.story.title}</h4>
-                <h6>By: {props.story.name}</h6>
-                <p>{props.story.story}</p>
-            </div>
+class EachSubmission extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            story: props.story,
+        }
+    }
 
-            <div className='approval'>
-                <i className="far fa-check-circle" onClick={event => props.updateStory(event, props.story)}></i>
-                <i className="far fa-times-circle" onClick={event => props.deleteStory(event, props.story)}></i>
-            </div>
-        </div >
-    )
+    toggleApproved = event => {
+        this.setState({
+            story: {
+                ...this.state.story,
+                isapproved: !this.state.story.isapproved
+            }
+        }, function () {
+            this.props.updateStory(event, this.state.story);
+        });
+    }
+
+    render() {
+        return (
+            <div className='story-container'>
+                <div className='story-header'>
+                    <div className='author-img-container'>
+                        <img className='author-img' src={this.props.story.imageurl} alt='Author' style={{maxHeight: "200px"}} />
+                    </div>
+
+                    <h4>{this.props.story.title}</h4>
+
+                    <h6>By: {this.props.story.name}</h6>
+
+                    <p>{this.props.story.story}</p>
+                </div>
+
+                <div className='approval'>
+                    <i
+                        className="fas fa-check"
+                        onClick={this.toggleApproved}
+                        style={this.state.story.isapproved ? { display: 'none' } : null}
+                    ></i>
+
+                    <i
+                        className="fas fa-undo"
+                        onClick={this.toggleApproved} 
+                        style={this.state.story.isapproved ? null : { display: 'none' }}
+                    ></i>
+
+                    <i
+                        className="far fa-trash-alt"
+                        onClick={event => this.props.deleteStory(event, this.props.story)}
+                    ></i>
+                </div>
+            </div >
+        )
+    }
 }
 
 export default EachSubmission;
--- stories/src/components/StorySubmissions/SubmissionPage.js
+++ stories/src/components/StorySubmissions/SubmissionPage.js
@@ -3,35 +3,31 @@ import axios from 'axios';
 import EachSubmission from './EachSubmission';
 import authenticate from '../Authentication/Authenticate';
 
-
-
 class SubmissionPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            submissions: []
+            submissions: [],
         };
     }
     
     componentDidMount() {
         axios
-        .get('https://refugee-stories-backend-rkolk.herokuapp.com/submissions')
-        .then(res => this.setState({ submissions: res.data }))
-        .catch(err => console.error(err));
+            .get('https://refugee-stories-backend-rkolk.herokuapp.com/submissions')
+
+            .then(res => this.setState({ submissions: res.data }))
+            
+            .catch(err => console.error(err));
     }
 
     updateStory = (event, element) => {
-        event.preventDefault();
-        console.log(this.state.submissions);
-        console.log(element);
+        event.persist();
+        
         axios
             .put(`https://refugee-stories-backend-rkolk.herokuapp.com/submissions/${element.id}`, element)
 
             .then(response => {
                 console.log(response);
-                this.setState({
-                    
-                })
                 window.location.reload();
             })
 
@@ -57,25 +53,43 @@ class SubmissionPage extends Component {
     }
 
     render() {
-        
+        const sorted = this.state.submissions.sort(function (a, b) {return a.id - b.id;});
+
         return (
             <div className='story-list-container'>
-            <h2>Submissions</h2>
-            {this.state.submissions.map(story => {
-                if (!story.isapproved) {
-                    return (
-                        <EachSubmission
-                            key={story.id}
-                            story={story}
-                            updateStory={this.updateStory}
-                            deleteStory={this.deleteStory}
-                        />
-                    )
-                }
-                
-                else return null;
-            })}
+                <h2>Current Submissions</h2>
 
+                {sorted.map(story => {
+                    if (!story.isapproved) {
+                        return (
+                            <EachSubmission
+                                key={story.id}
+                                story={story}
+                                updateStory={this.updateStory}
+                                deleteStory={this.deleteStory}
+                            />
+                        )
+                    }
+
+                    else return null;
+                })}
+
+                <h2>Approved Submissions</h2>
+
+                {sorted.reverse().map(story => {
+                    if (story.isapproved) {
+                        return (
+                            <EachSubmission
+                                key={story.id}
+                                story={story}
+                                updateStory={this.updateStory}
+                                deleteStory={this.deleteStory}
+                            />
+                        )
+                    }
+
+                    else return null;
+                })}
             </div>
         )
     }
