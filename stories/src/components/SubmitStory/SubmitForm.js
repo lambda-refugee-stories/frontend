63217c813613e7faf182f9fb7a7957179f147e64
Nicole Gunther <nicolemgunther@gmail.com>
2019-03-14 14:55:06 -0700

    styled submitpage and submitform files

--- stories/src/components/SubmitStory/SubmitForm.js
+++ stories/src/components/SubmitStory/SubmitForm.js
@@ -3,38 +3,49 @@ import React from 'react';
 const SubmitForm = props => {
     return (
         <div>
-            <form onSubmit={props.submitStory} autoComplete="off">
-                <input
-                    placeholder="Name"
-                    type="text"
-                    name="name"
-                    value={props.submission.name}
-                    onChange={props.handleChanges}
-                ></input>
+            <form
+                className='submit-form'
+                onSubmit={props.submitStory}
+                autoComplete="off"
+            >
+                <div className="short-inputs">
+                    <input
+                        className='submit-input'
+                        placeholder="Name (or Anonymous)"
+                        type="text"
+                        name="name"
+                        value={props.submission.name}
+                        onChange={props.handleChanges}
+                    ></input>
 
-                <input
-                    placeholder="Enter a title"
-                    type="text"
-                    name="title"
-                    value={props.submission.title}
-                    onChange={props.handleChanges}
-                ></input>
+                    <input
+                        className='submit-input'
+                        placeholder="Enter a title"
+                        type="text"
+                        name="title"
+                        value={props.submission.title}
+                        onChange={props.handleChanges}
+                    ></input>
 
-                <input
-                    placeholder="Paste image URL"
-                    type="text"
-                    name="imageurl"
-                    value={props.submission.imageurl}
-                    onChange={props.handleChanges}
-                ></input>
+                    <input
+                        className='submit-input'
+                        placeholder="Paste image URL"
+                        type="text"
+                        name="imageurl"
+                        value={props.submission.imageurl}
+                        onChange={props.handleChanges}
+                    ></input>
+                </div>
 
-                <input
+                <textarea
+                    className='submit-input long-input'
                     placeholder="Enter Your Story Here"
                     type="text"
                     name="story"
+                    wrap="hard"
                     value={props.submission.story}
                     onChange={props.handleChanges}
-                ></input>
+                ></textarea>
 
                 <button type="submit">Submit</button>
             </form>
